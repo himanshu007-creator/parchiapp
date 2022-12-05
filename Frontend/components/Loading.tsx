f336b3dbd06da1da14aae1650697bc36d0e5fc72
himanshu007-creator <addyjeridiq@gmail.com>
2022-12-05 19:02:58 +0530

    [UI] loading positioning

--- Frontend/components/Loading.tsx
+++ Frontend/components/Loading.tsx
@@ -5,7 +5,7 @@ const Loading:React.FC = ()=>{
     return(
         <>
         {/* <div className=" w-full h-screen backdrop-blur-sm"> */}
-            <div  className={`absolute top-3/6  lg:left-[40%] lg:top-80 left-8 top-36 inset-x-42  pr-8 pl-8 pb-8 pt-4 py-6 mt-2 text-left  flex items-center justify-center  rounded-xl  ${theme==='dark' ? 'bg-transparent border-4 border-red-400 shadow-red-200':'bg-white'} shadow-lg w-80 h-80`}>
+            <div  className={`absolute top-3/6  lg:left-[40%] lg:top-80 left-[6%] top-36 inset-x-42  pr-8 pl-8 pb-8 pt-4 py-6 mt-2 text-left  flex items-center justify-center  rounded-xl  ${theme==='dark' ? 'bg-transparent border-4 border-red-400 shadow-red-200':'bg-white'} shadow-lg w-80 h-80`}>
             LOADING
             </div>
         {/* </div> */}
