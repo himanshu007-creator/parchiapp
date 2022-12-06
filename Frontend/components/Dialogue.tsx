392633c69b200c8d4e03fa84b7366f4b64b4091d
himanshu007-creator <addyjeridiq@gmail.com>
2022-12-07 01:10:39 +0530

    UI fix

--- Frontend/components/Dialogue.tsx
+++ Frontend/components/Dialogue.tsx
@@ -10,7 +10,7 @@ const Dialogue:React.FC<DialogueProps> = ({show}: DialogueProps)=>{
         <>
         {
             show ?
-            <div  className={`absolute top-[29%] items-center justify-center lg:left-[42%] lg:top-80 left-[16%] top-70 inset-x-42  pr-8 pl-8 pb-8 pt-4 py-6 mt-2 text-left  flex items-center justify-center  rounded-xl backdrop-blur-2xl ${theme==='dark' ? ' border-4 border-red-400 shadow-red-200':''} shadow-lg w-80 h-80`}>
+            <div  className={`absolute top-[29%] items-center justify-center lg:left-[42%] lg:top-80 left-[8%] top-70 inset-x-42  pr-8 pl-8 pb-8 pt-4 py-6 mt-2 text-left  flex items-center justify-center  rounded-xl backdrop-blur-2xl ${theme==='dark' ? ' border-4 border-red-400 shadow-red-200':''} shadow-lg w-80 h-80`}>
             <Image src="/img/loading.svg" alt="me" width="80" height="80" />           
             </div>:
             <></>
--- Frontend/pages/dashboard.tsx
+++ Frontend/pages/dashboard.tsx
@@ -30,8 +30,13 @@ const Dashboard: React.FC = () => {
   }
 
   return <>
-    <div className={`${loading? 'pointer-events-none':''} flex flex-wrap h-screen bg-gray-100 ${theme === 'dark' ? 'bg-zinc-900' : 'bg-gray-100 overflow-hidden'}`}>
+    <div className={`${loading? 'pointer-events-none':''} flex flex-wrap h-screen bg-gray-100  ${theme === 'dark' ? 'bg-zinc-900' : 'bg-gray-100 overflow-hidden'}`}>
       <div className={`bg-red-500 w-full h-16 px-4 rounded-b-lg ${theme === 'dark' ? 'shadow-red-500/50 shadow-lg' : ''}`}>
+        <div className={`float-left lg:ml-6 mt-2 w-4/6 h-4/6 bg-gradient-to-r ${theme === 'dark'?'from-gray-700':'from-violet-700'} to-green-300 animate-pulsate rounded-lg`}>
+          <p className="font-bold py-2 px-8 mb-2 text-2xl font-abril-fatface">
+            ParchiApp
+          </p>
+        </div>
         <div onMouseEnter={() => { console.log("HOVER") }} onClickCapture={() => { setLogoutVisible(!logout) }} onMouseLeave={() => { setTimeout(() => { setLogoutVisible(false) }, 2500) }} className={`group br-1 p-2 text-2xl bg-gray-300 rounded-lg  hover:bg-gray-500 w-16 h-full float-right`}>
           <div className='p-2 px-4 bg-red-600 h-full w-full rounded-full'>
             <p className='visible group-hover:invisible '>H</p>
@@ -40,9 +45,9 @@ const Dashboard: React.FC = () => {
         </div>
       </div>
       <div className='w-full h-full bg-transparent lg:px-4'>
-        {
+      {
           logout ?
-            <div onClick={()=>logoutSubmit} className={`transition ease-in-out absolute  h-14 w-60 bg-red-300 p-2 ${theme === 'dark' ? 'bg-black shadow-red-500/50 shadow-lg' : ''} rounded-lg right-2`}>
+            <div onClick={()=>logoutSubmit} className={`transition ease-in-out absolute  h-14 w-60 bg-red-300 p-2 ${theme === 'dark' ? 'bg-black shadow-red-500/50 shadow-lg' : ''} rounded-lg right-2 z-50`}>
               <button className='h-full w-full border-2 border-current p-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-left">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
@@ -53,9 +58,8 @@ const Dashboard: React.FC = () => {
             :
             <></>
         }
-
-        <div className='w-full h-full mx-4 lg:p-8 bg-green-100 '>
-          <button onClick={() => { console.log("hey") }} className='w-2/6 lg:w-1/6 h-8 lg:h-6 lg:h-12 border-2  border-black float-right rounded-lg lg:px-8 lg:py-2 px-4 lg:mx-2'>
+        <div className='w-full h-full  lg:p-8 bg-green-100 p-3'>
+          <button onClick={() => { console.log("hey") }} className='lg:mb-4 w-2/6 lg:w-1/6 h-8 lg:h-6 lg:h-12 border-2  border-black float-right rounded-lg lg:px-8 lg:py-2 px-2 lg:mx-2 relative right-4 bg-cyan-500'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 float-left">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
@@ -72,12 +76,11 @@ const Dashboard: React.FC = () => {
                   // eslint-disable-next-line react/jsx-key
                   <FileOptions
                     show={i === 1}
-                    preventMouseEvents={loading}
                     >
-                    <div className='p-2 lg:px-4 lg: py-2 my-3 w-full h-28 bg-red-200 rounded-lg flex '>
+                    <div className={`p-2 lg:px-4 lg: py-2 my-3 w-full h-28 ${theme !== 'dark' ?'bg-red-200':'bg-black'} rounded-lg flex`}>
                       <Image src="/img/musk.jpeg" alt='' className='h-24 w-48 border-2 fixed ' width={100} height={48} />
                       <div className='w-full h-full flex flex-wrap px-4'>
-                        <p className='font-bold font-sans w-full'>AA meri jaan</p>
+                        <p className='font-bold font-sans w-full'>AAAAAA</p>
                         <p className='font-medium'>Access Holders: Elon Musk</p>
                       </div>
                     </div>
